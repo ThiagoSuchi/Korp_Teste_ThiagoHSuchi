4fdc006c3127bfcd430a6774174bc95dbb14ec2f
ThiagoSuchi <thiagohenssuchi@gmail.com>
2025-11-07 13:38:07 -0400

    Expandindo  o contrato adicionando operações assíncronas para obter e debitar produtos.

--- backend/EstoqueService/Repositories/IProductRepository.cs
+++ backend/EstoqueService/Repositories/IProductRepository.cs
@@ -6,4 +6,6 @@ public interface IRepositorioProduto
 {
 	Task<bool> TentarAdicionarAsync(Produto produto, CancellationToken cancellationToken);
 	Task<IReadOnlyCollection<Produto>> ObterTodosAsync(CancellationToken cancellationToken);
+	Task<Produto?> ObterPorCodigoAsync(string codigo, CancellationToken cancellationToken);
+	Task<(bool Sucesso, string? Erro)> TentarDebitarAsync(IReadOnlyCollection<(Produto produto, int quantidade)> consumos, CancellationToken cancellationToken);
 }
